deba96d06192fbc328db398ead128410cb27f16c
Fábio Pereira <fjopereira@gmail.com>
2025-03-17 20:18:45 +0000

    adiciona scripts ´lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home(){
-    return <h1>Mensagem correta</h1>
+function Home() {
+  return <h1>Mensagem correta</h1>;
+}
+
+function teste() {
+  console.log("teste indentacao");
 }
 
 export default Home;
-
